0102e94c7f8d48058b4cc5207a8b0d8b575527b6
vikoss <vikoss.yiss@gmail.com>
2021-06-22 20:15:44 -0500

    Services account of firebase 👨‍🔧️

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,10 +1,16 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-import serviceAccount from '~/secret/firebase'
 
 if (!firebase.apps.length) {
-  firebase.initializeApp(serviceAccount)
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCdK3rqE1Au0P0Bd8Rt9Eg93qwLMKgNnS8',
+    authDomain: 'trello-clone-f5cd7.firebaseapp.com',
+    projectId: 'trello-clone-f5cd7',
+    storageBucket: 'trello-clone-f5cd7.appspot.com',
+    messagingSenderId: '733969222361',
+    appId: '1:733969222361:web:043d28cbd10901ee048cc8'
+  })
 }
 
 const auth = firebase.auth()
